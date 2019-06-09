7504ca9a6a986dd381b3f56e8b5c090f20d62707
root <mr.naasso@gmail.com>
2019-06-09 18:25:43 +0200

    add basic functionality to load yaml files

--- db/source.go
+++ db/source.go
@@ -1,1 +1,22 @@
 package db
+
+import (
+	"gopkg.in/yaml.v2"
+	"os"
+)
+
+func loadYaml(dst interface{}, yamlPath string) error {
+	var (
+		err error
+		f   *os.File
+	)
+	// open yaml file
+	if f, err = os.OpenFile(yamlPath, os.O_RDONLY, 0644); err != nil {
+		return err
+	}
+	//decode into our destination in strict mode
+	dec := yaml.NewDecoder(f)
+	dec.SetStrict(true)
+	defer f.Close()
+	return dec.Decode(dst)
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/shaban/polaris
 
 go 1.12
+
+require gopkg.in/yaml.v2 v2.2.2
